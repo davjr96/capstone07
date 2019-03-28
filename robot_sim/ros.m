069cb6279b70833568e0f501280c62ff7da07668
Danny Voce <davjr96@gmail.com>
2019-03-28 12:25:49 -0400

    Added straight driving

--- robot_sim/ros.m
+++ robot_sim/ros.m
@@ -1,26 +1,42 @@
 ipaddress = '192.168.8.250';
 rosinit(ipaddress)
 
+%vars for traveling forward
+target_distance = 1;
+linear_speed = .1; %m/s
+distance_travelled = 0;
+
+%vars for rotating
 current_theta = 0;
 target_theta = 3.14;
 relative_angle = target_theta - current_theta;
 current_angle = 0;
 angular_speed = .5; %rads/s
+
 robot = rospublisher('/mobile_base/commands/velocity') ;
 velmsg = rosmessage(robot);
-velmsg.Angular.Z = angular_speed;
-t0 = cputime;
 r = rosrate(10);
-reset(r);
 
-counter = 1;
+reset(r);
+velmsg.Angular.Z = angular_speed;
 while(current_angle < relative_angle)
    time = r.TotalElapsedTime;
    send(robot,velmsg);
-   current_angle = angular_speed * (.1 * counter)
-   counter = counter + 1;
+   current_angle = current_angle + angular_speed * .1
    waitfor(r);
 end
+velmsg.Angular.Z = 0;
+
+reset(r);
+
+velmsg.Linear.X = linear_speed;
+while(distance_travelled < target_distance)
+   time = r.TotalElapsedTime;
+   send(robot,velmsg);
+   distance_travelled = distance_travelled + linear_speed * .1
+   waitfor(r);
+end
+
 rosshutdown;
 
  
