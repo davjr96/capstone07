a97be8b950f8be39a5f824b4ea80e9e50e3cbcc6
Danny Voce <davjr96@gmail.com>
2019-03-28 12:39:51 -0400

    Seperate to functions

--- robot_sim/ros.m
+++ robot_sim/ros.m
@@ -1,43 +1,45 @@
 ipaddress = '192.168.8.250';
 rosinit(ipaddress)
 
-%vars for traveling forward
-target_distance = 1;
-linear_speed = .1; %m/s
-distance_travelled = 0;
-
-%vars for rotating
-current_theta = 0;
-target_theta = 3.14;
-relative_angle = target_theta - current_theta;
-current_angle = 0;
-angular_speed = .5; %rads/s
-
 robot = rospublisher('/mobile_base/commands/velocity') ;
-velmsg = rosmessage(robot);
-r = rosrate(10);
 
-reset(r);
-velmsg.Angular.Z = angular_speed;
-while(current_angle < relative_angle)
-   time = r.TotalElapsedTime;
-   send(robot,velmsg);
-   current_angle = current_angle + angular_speed * .1
-   waitfor(r);
-end
-velmsg.Angular.Z = 0;
-
-reset(r);
-
-velmsg.Linear.X = linear_speed;
-while(distance_travelled < target_distance)
-   time = r.TotalElapsedTime;
-   send(robot,velmsg);
-   distance_travelled = distance_travelled + linear_speed * .1
-   waitfor(r);
-end
-
+rotate(3.14, robot)
+drive(1 ,robot)
+    
 rosshutdown;
 
- 
+function rotate (relative_angle, pub)
+    current_angle = 0;
+    angular_speed = .5; %rads/s
+    r = rosrate(10);
+    reset(r);
+    velmsg = rosmessage(pub);
+    velmsg.Angular.Z = angular_speed;
+    while(current_angle < relative_angle)
+       time = r.TotalElapsedTime;
+       send(pub,velmsg);
+       current_angle = current_angle + angular_speed * .1
+       waitfor(r);
+    end
+    velmsg.Angular.Z = 0;
+end
+
+function drive(distance, pub)
+    %vars for traveling forward
+    target_distance = 1;
+    linear_speed = .1; %m/s
+    distance_travelled = 0;
+    r = rosrate(10);
+    reset(r);
+    velmsg = rosmessage(pub);
+    velmsg.Linear.X = linear_speed;
+    while(distance_travelled < target_distance)
+       time = r.TotalElapsedTime;
+       send(pub,velmsg);
+       distance_travelled = distance_travelled + linear_speed * .1
+       waitfor(r);
+    end
+end
+
+
      
